2794f3df55476fd6469a904cb075fa158df22dbd
Dimitrios Zaras <dimitri.zaras@gmail.com>
2022-11-30 16:23:29 -0500

    Added code that included cases from folders 10.03.22 and 11.07.22

--- joining_dids_to_be_uploaded_to_rc_with_extracted_variables_data.R
+++ joining_dids_to_be_uploaded_to_rc_with_extracted_variables_data.R
@@ -12,9 +12,15 @@ library(tidyverse)
 setwd('C:/Users/DC20B46/Documents')
 
 ## Import extracted data from local drive
-df1 <- read_csv('extracted_variables_from_SUDORS_narratives_2022_11-28-22.csv')
+df1a <- read_csv('extracted_variables_from_SUDORS_narratives_2019-2022_11-28-22.csv')
 
-df1$DID <- as.numeric(df1$DID)  # convert to numeric so that there are no issues with the joining dataframes
+## Now, I will import the cases from the folders 10.03.22 and 11.07.22 on the Z: drive
+df1b <- read_csv('extracted_variables_from_SUDORS_narratives_10.03.22_11.07.22.csv')
+
+# Concatenate the two dataframes df1a and df1b
+df1c <- rbind(df1a, df1b)
+
+df1c$DID <- as.numeric(df1c$DID)  # convert to numeric so that there are no issues with the joining dataframes
 
 ## import list of DIDs provided on 11/28/22 that are to be imported to REDCAP
 df2 <- read_csv('C:/Users/DC20B46/Downloads/DIDs_to_scrape_import.csv')
@@ -22,14 +28,21 @@ df2 <- read_csv('C:/Users/DC20B46/Downloads/DIDs_to_scrape_import.csv')
 df2$DID <- df2$did  # create a new column 'DID' to match the name of the column in the df1 dataframe
 df2 = subset(df2, select = -c(did)) # drop column 'did' for clarity
 
-df3 <- left_join(df2, df1, by = "DID")  # Apply left_join to keep only data that match DIDs from df2
+df3 <- left_join(df2, df1c, by = "DID")  # Apply left_join to keep only data that match DIDs from df2
 
-sum(is.na(df3$...1))  # 213 NAs 
-sum(is.na(df3$year))  # 213 NAs
-## So, we seem to have 297 cases for which we have values that we can upload to REDCAP 
-## (510 observations in df3 - 213 rows with NAs)
+sum(is.na(df3$...1))  # Only 1 NA after concatenating df1a and df1b - when joining only df1a we had 213 NAs 
+sum(is.na(df3$year))  # 1 NA
+sum(is.na(df3$hxfentanyl)) # 18 NAs
+sum(is.na(df3$narr)) # 28 NAs - so even if we upload 492 cases, we might really have 482
 
-write.csv(df3, 'C:/Users/DC20B46/Documents', row.names = F)
+## So, we seem to have 280 cases for which we have values that we can upload to REDCAP 
+## (510 observations in df3 - 230 rows with NAs)
+
+which(df3$cme_suicideattempthistory==1)
+
+df3[123,]
+
+write.csv(df3, 'C:/Users/DC20B46/Documents/extracted_variables_combined_until_11.07.22.csv', row.names=F)
 
 
 
